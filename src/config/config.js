408fe91638ebe1a8921aeabaea5c2374b6dde8c6
yanglaiwei <yanglw@szkexin.com.cn>
2019-05-02 18:57:21 +0800

    no message

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+    commonPwd: "wufawutian", //登录的通用密码
+    baseUrl: {
+      dev: 'http://www.canteen.com/api/',//后台的地址
+      //dev: "localhost:8181",
+      pro: ""
+    },
+    //cookies里保存时间默认是一天
+    cookieExpires: 7
+  };
+  export default config;
