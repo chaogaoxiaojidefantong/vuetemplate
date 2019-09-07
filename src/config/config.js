b2e9f7a9d8dbed2d7429ef41f2fbb8d141a275b5
ylw <759646095@qq.com>
2019-09-07 10:19:41 +0800

    lll

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const config = {
       dev: 'http://www.lcw.com/api/',//后台的地址      
       pro: ""
     },
-    //cookies里保存时间默认是一天
+    //cookies里保存时间默认是一
     cookieExpires: 7
   };
   export default config;
